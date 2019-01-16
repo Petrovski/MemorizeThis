8674d2a365a55b4f034763a49227736417a34729
FORTRESS <alexpetroski@gmail.com>
2019-01-15 23:23:20 -0500

    Updates

---
+++ precache-manifest.c15231fb1ebea2f63e58c1fe0d9927a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e2fa784949504e7ec4478dad61f7cb",
+    "url": "/MemorizeThis/static/media/overwatch-background.89e2fa78.jpg"
+  },
+  {
+    "revision": "54462bf7eb352ea31fb1",
+    "url": "/MemorizeThis/static/js/runtime~main.54462bf7.js"
+  },
+  {
+    "revision": "a22abec6a460025e7ed6",
+    "url": "/MemorizeThis/static/js/main.a22abec6.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/MemorizeThis/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "a22abec6a460025e7ed6",
+    "url": "/MemorizeThis/static/css/main.6631adf5.chunk.css"
+  },
+  {
+    "revision": "1692477e05e7be1ec9324a949131ecda",
+    "url": "/MemorizeThis/index.html"
+  }
+];
